0abf819762f255ad316f05b48f7df490fd130cfc
berniehm <bernieh1012008@live.co.uk>
2018-08-27 19:02:01 +0100

    delete function and user list activity began

---
+++ app/src/main/java/org/wit/mytwitterverfour/activties/TweeterListActivity.java
@@ -0,0 +1,19 @@
+package org.wit.mytwitterverfour.activties;
+
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.ListView;
+
+import org.wit.mytwitterverfour.main.MyTweetApp;
+
+public class TweeterListActivity extends AppCompatActivity {
+
+        private ListView listView;
+        private MyTweetApp app;
+      //  private UserAdapter adapter;
+        private Button friends;
+
+
+
+
+}
--- app/src/main/java/org/wit/mytwitterverfour/model/User.java
+++ app/src/main/java/org/wit/mytwitterverfour/model/User.java
@@ -7,12 +7,14 @@ package org.wit.mytwitterverfour.model;
  * https://wit-ictskills-2017.github.io/mobile-app-dev/topic02-b/book-a-donation-03/index.html#/Donation-03
  */
 public class User {
+    public String id;
     public String firstName;
     public String lastName;
     public String email;
     public String password;
 
-    public User(String firstName, String lastName, String email, String password) {
+    public User(String id ,String firstName, String lastName, String email, String password) {
+        this.id = id ;
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
